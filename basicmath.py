55972e82b365c8b4714cbf844fdf0622dcbcfbd3
jon-maddocks <56932287+jon-maddocks@users.noreply.github.com>
2022-07-26 15:28:40 -0400

    First iteration of basic math function added

---
+++ basicmath.py
@@ -0,0 +1,19 @@
+#Add two numbers and return the result
+def doAdd(num1, num2):
+    answer = num1 + num2
+    return answer
+
+#Subtract two number and return the result
+def doSub(num1, num2):
+    answer = num1 - num2
+    return answer
+
+#Multiply two numbers and return the result
+def doMul(num1, num2):
+    answer = num1 * num2
+    return answer
+
+#Divide two numbers and return the result
+def doDivide(num1, num2):
+    answer = num1 / num2
+    return answer
